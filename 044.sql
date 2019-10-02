19e35a9d48b0af00eeb4022ec9240bbb7d6e60ec
donxd <n0s3.xd@gmail.com>
2019-10-02 15:41:44 -0500

    initial commit of arcade-db/044

---
+++ 044.sql
@@ -0,0 +1,5 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE scholarshipsDistribution()
+BEGIN
+    SELECT candidate_id as student_id FROM candidates WHERE candidate_id NOT IN (SELECT student_id FROM detentions);
+END
